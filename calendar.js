e92d43e23dd09fca6dad2f5628e10d43a2474652
qwezsd <avwds@naver.com>
2024-02-13 21:43:57 +0900

    golfus

---
+++ calendar.js
@@ -0,0 +1,61 @@
+
+let date = new Date();
+
+const renderCalendar = ( )=> {
+    const viewYear = date.getFullYear();
+    const viewMonth = date.getMonth();
+    console.log(viewMonth)
+
+    document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth+1}월`
+
+
+    const prevLast = new Date(viewYear, viewMonth, 0);
+    const thisLast = new Date(viewYear, viewMonth+1, 0);
+
+
+
+    const PLDate = prevLast.getDate();
+    const PLDay = prevLast.getDay();
+
+    const TLDate = thisLast.getDate();
+    const TLDay = thisLast.getDay();
+
+    const prevDates = [];
+    const thisDates = [...Array(TLDate+1).keys()].slice(1);
+    const nextDates = [];
+
+    if(PLDay !== 6){
+        for (let i = 0; i<PLDay+1; i++){
+            prevDates.unshift(PLDate - i)
+        }
+    }
+
+    for(let i =1; i<7-TLDay; i++){
+        nextDates.push(i);
+    }
+
+    const dates = prevDates.concat(thisDates, nextDates);
+
+    dates.forEach((date, i) => {
+        dates[i] = `<div class="date">${date}</div>`
+    })
+
+    document.querySelector('.dates').innerHTML = dates.join('');
+}
+
+renderCalendar();
+
+const prevMonth = () => {
+    date.setMonth(date.getMonth()-1);
+    renderCalendar();
+};
+
+const nextMonth = () => {
+    date.setMonth(date.getMonth() + 1);
+    renderCalendar();
+}
+
+const goToday = () => {
+    date = new Date();
+    renderCalendar();
+}
--- isBg.js
+++ isBg.js
@@ -1,32 +1,31 @@
+
 var bgColor = document.querySelector('#wrap')
 var headerColor = document.querySelector('#header')
 var reserveColor = document.querySelector('#reserve')
 
 
 document.addEventListener("scroll", ()=>{
-    const screenY = window.scrollY;
+const screenY = window.scrollY;
 
-    console.log(screenY);
 
-            if (screenY >=1492 && 2124 >= screenY) {
-                bgColor.classList.add("gray");
-                bgColor.classList.remove("black");
-                headerColor.classList.remove("black");
-            }
-            else if (screenY > 2124) {
-                bgColor.classList.add("black");
-                bgColor.classList.remove("gray");
-                headerColor.classList.add("black");
-                reserveColor.classList.add("black");
-                reserveColor.classList.remove("white");
-            }
-            else {
-                bgColor.classList.remove("gray");
-                bgColor.classList.remove("black");
-                headerColor.classList.remove("black");
-                reserveColor.classList.remove("black");
-                reserveColor.classList.add("white");
-            }
-    })
 
-    //1641
+if (screenY >=1492 && 2124 >= screenY) {
+        bgColor.classList.add("gray");
+        bgColor.classList.remove("black");
+        headerColor.classList.remove("black");
+    }
+    else if (screenY > 2124) {
+        bgColor.classList.add("black");
+        bgColor.classList.remove("gray");
+        headerColor.classList.add("black");
+        reserveColor.classList.add("black");
+        reserveColor.classList.remove("white");
+    }
+    else {
+        bgColor.classList.remove("gray");
+        bgColor.classList.remove("black");
+        headerColor.classList.remove("black");
+        reserveColor.classList.remove("black");
+        reserveColor.classList.add("white");
+    }
+})
